8cbcaa4d04f7f239b0c3f55efda6a039cdec31de
unknown <marcin.leganowski>
2018-01-21 20:21:12 +0100

    Stworzenie projektu strony

---
+++ ETL Ceneo/ETL Ceneo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETL_Ceneo.Startup))]
+namespace ETL_Ceneo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
