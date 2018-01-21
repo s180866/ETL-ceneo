e9576c122607028b99d5aec6f82f1110d6f10622
unknown <marcin.leganowski>
2018-01-21 20:27:43 +0100

    Dodanie klasy produkt

---
+++ ETL Ceneo/ETL Ceneo/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL_Ceneo
+{
+    class Product
+    {
+        //Klasa do przechowywania informacji o produktach
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string DeviceType { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+
+        public int Recomend { get; set; }
+        public int NotRecomendNumber { get; set; }
+
+        public double Stars { get; set; }
+        public string Id { get; set; }
+        public int IdNumeric { get; set; }
+    }
+}
