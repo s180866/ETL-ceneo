d0901a852bc45cfb3ae1f0ce59d5ed6a236420f8
unknown <marcin.leganowski>
2018-01-21 20:30:45 +0100

    LoadData konsturktor

---
+++ ETL Ceneo/ETL Ceneo/App_Code/LoadData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Opis podsumowujący dla LoadData
+/// </summary>
+public class LoadData
+{
+    public LoadData()
+    {
+        //
+        // TODO: Tutaj dodaj logikę konstruktora
+        //
+    }
+}
