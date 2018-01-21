f8b825648157d45ac3a8ee2ad13c05de57fb16fd
unknown <marcin.leganowski>
2018-01-21 20:42:05 +0100

    Instalacja pakietu HTML Agility Pack w celu pobierania tresci

--- ETL Ceneo/ETL Ceneo/App_Code/LoadData.cs
+++ ETL Ceneo/ETL Ceneo/App_Code/LoadData.cs
@@ -14,22 +14,22 @@ public class LoadData
     }
     public static string LoadDataToDB(string id)
     {
-        var IdList = MySqlConnector.GetDataFromMySQLArray("SELECT ProductId FROM etl_ceneo.product;", "ProductId");
-        var IdListOpinion = MySqlConnector.GetDataFromMySQLArray("SELECT OpinionId FROM etl_ceneo.opinion;", "OpinionId");
+        var IdList = ETL_Ceneo.MySqlConnector.GetDataFromMySQLArray("SELECT ProductId FROM etl_ceneo.product;", "ProductId");
+        var IdListOpinion = ETL_Ceneo.MySqlConnector.GetDataFromMySQLArray("SELECT OpinionId FROM etl_ceneo.opinion;", "OpinionId");
 
         //Udate/Insert Product
         if (IdList.Contains(id))
-            MySqlConnector.InsertProduct(TransformData.product_transformed_dicc[id], "update");
+            ETL_Ceneo.MySqlConnector.InsertProduct(ETL_Ceneo.TransformData.product_transformed_dicc[id], "update");
         else
-            MySqlConnector.InsertProduct(TransformData.product_transformed_dicc[id], "insert");
+            ETL_Ceneo.MySqlConnector.InsertProduct(ETL_Ceneo.TransformData.product_transformed_dicc[id], "insert");
 
         //Update/Insert Opinion
         if (IdListOpinion.Contains(id))
-            MySqlConnector.InsertOpinion(TransformData.opinions_transformed_dicc[id], "update");
+            ETL_Ceneo.MySqlConnector.InsertOpinion(ETL_Ceneo.TransformData.opinions_transformed_dicc[id], "update");
         else
-            MySqlConnector.InsertOpinion(TransformData.opinions_transformed_dicc[id], "insert");
+            ETL_Ceneo.MySqlConnector.InsertOpinion(ETL_Ceneo.TransformData.opinions_transformed_dicc[id], "insert");
 
-        return string.Concat("Załadowano ", MySqlConnector.CountInsertedRows.ToString(), " oraz zaktualizowano ", MySqlConnector.CountUpdatedRows.ToString(), " opinii dla produktu o ID: ", id);
+        return string.Concat("Załadowano ", ETL_Ceneo.MySqlConnector.CountInsertedRows.ToString(), " oraz zaktualizowano ", ETL_Ceneo.MySqlConnector.CountUpdatedRows.ToString(), " opinii dla produktu o ID: ", id);
     }
     public static int CountInsertedRows { get; set; }
     public static int CountUpdatedRows { get; set; }
